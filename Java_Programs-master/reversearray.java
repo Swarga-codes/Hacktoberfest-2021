e867293adcfe87cfffca03bfa515ec9f72f80d30
SwargaRajDutta <72154312+Swarga-codes@users.noreply.github.com>
2021-10-16 00:23:09 +0530

    reversed the elements of the array

---
+++ Java_Programs-master/reversearray.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class ReverseArray{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        int ar[]=new int[n];
+        System.out.println("Enter array elements");
+        for(int i=0;i<n;i++){
+            ar[i]=sc.nextInt();
+        }
+        System.out.println("The reversed array is:");
+        for(int i=(n-1);i>=0;i--){
+            System.out.println(ar[i]+" ");
+        }
+        
+    }
+}
